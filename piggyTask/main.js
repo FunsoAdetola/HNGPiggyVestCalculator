15107792924add7c59545eea76613100f8482b8d
felistus <felistusobieze@gmail.com>
2020-06-18 09:38:33 +0100

    update UI and functionality

---
+++ piggyTask/main.js
@@ -0,0 +1,18 @@
+$(document).ready( () => {
+    // $(function () {
+    //     $('[data-toggle="tooltip"]').tooltip()
+    //   })
+
+    //   $('#popUp').modal({
+    //     show: false,
+    //   })
+
+    $(".calc").click( () => {
+        
+        let percentage = $(this).find(".interestPercentage").text(); console.log(percentage)
+        // let month = $(".investMonth").text(); console.log(month)
+        console.log("dsksdks")
+    });
+
+
+})
